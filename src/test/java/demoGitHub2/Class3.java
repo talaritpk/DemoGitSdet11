26a002b6a26d572a3705cc2dc13489304722b5a9
pavan kumar <talaritpk@gmail.com>
2023-01-04 17:08:55 +0530

    commit2

---
+++ src/test/java/demoGitHub2/Class3.java
@@ -0,0 +1,17 @@
+package demoGitHub2;
+
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class Class3 {
+	@Test
+	public void tyss()
+	{
+		System.out.println("I am an QAE");
+		Random random=new Random();
+		int i=random.nextInt(1000);
+		System.out.println(i);
+		
+	}
+}
---
+++ src/test/java/demoGitHub2/Class4.java
@@ -0,0 +1,19 @@
+package demoGitHub2;
+
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class Class4 {
+	
+	@Test
+	public void tyss()
+	{
+		System.out.println("I am an QA");
+		Random random=new Random();
+		int i=random.nextInt(1000);
+		System.out.println(i);
+		
+	}
+
+}
