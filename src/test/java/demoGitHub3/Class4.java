fd90c3637b7f2cf2ad2fa3a7e95e1bdc503b1976
pavan kumar <talaritpk@gmail.com>
2023-01-04 17:12:57 +0530

    commit3

---
+++ src/test/java/demoGitHub3/Class4.java
@@ -0,0 +1,18 @@
+package demoGitHub3;
+
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class Class4 {
+	@Test
+	public void tyss()
+	{
+		System.out.println("I am an QA");
+		Random random=new Random();
+		int i=random.nextInt(1000);
+		System.out.println(i);
+		
+	}
+
+}
