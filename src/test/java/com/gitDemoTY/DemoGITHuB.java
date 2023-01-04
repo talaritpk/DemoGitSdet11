6fd329bc491451cc2ad6f8efe09b2d0285334ec5
tpkta <tpkta@PAVANKUMAR>
2023-01-04 16:11:05 +0530

    Hello Sdet11 commit

---
+++ src/test/java/com/gitDemoTY/DemoGITHuB.java
@@ -0,0 +1,17 @@
+package com.gitDemoTY;
+
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class DemoGITHuB {
+	@Test
+	public void tyss()
+	{
+		System.out.println("I am an SDET");
+		Random random=new Random();
+		int i=random.nextInt(1000);
+		System.out.println(i);
+		
+	}
+}
---
+++ src/test/java/com/gitDemoTY/DemoGITHuB2.java
@@ -0,0 +1,17 @@
+package com.gitDemoTY;
+
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class DemoGITHuB2 {
+
+	@Test
+	public void tyss() {
+
+		System.out.println("I am an AUTOMATION ENGINEER");
+		Random random = new Random();
+		int i = random.nextInt(200);
+		System.out.println(i);
+	}
+}
