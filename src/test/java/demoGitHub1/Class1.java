fa4051b2f8e063a6071a966042415df3991d863d
pavan kumar <talaritpk@gmail.com>
2023-01-04 16:58:12 +0530

    commit1

---
+++ src/test/java/demoGitHub1/Class1.java
@@ -0,0 +1,18 @@
+package demoGitHub1;
+
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	
+	@Test
+	public void tyss()
+	{
+		System.out.println("I am an SDET");
+		Random random=new Random();
+		int i=random.nextInt(1000);
+		System.out.println(i);
+		
+	}
+}
---
+++ src/test/java/demoGitHub1/Class2.java
@@ -0,0 +1,19 @@
+package demoGitHub1;
+
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+	
+	@Test
+	public void tyss()
+	{
+		System.out.println("I am an AE");
+		Random random=new Random();
+		int i=random.nextInt(1000);
+		System.out.println(i);
+		
+	}
+
+}
